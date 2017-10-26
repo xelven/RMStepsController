30f77fa9319ac5efb2ae259f938f9f2cb7276bec
Scott Chou <scott.ph.chou@gmail.com>
2017-10-26 12:30:13 +0800

    Fix iOS 11 ui glitch

--- RMStepsController/RMStepsController.m
+++ RMStepsController/RMStepsController.m
@@ -63,13 +63,17 @@
     
     NSDictionary *bindingsDict = NSDictionaryOfVariableBindings(stepsBar, container);
     
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[stepsBar]" options:0 metrics:nil views:bindingsDict]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[container]-(0)-|" options:0 metrics:nil views:bindingsDict]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-0-[stepsBar]-0-|" options:0 metrics:nil views:bindingsDict]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-0-[container]-0-|" options:0 metrics:nil views:bindingsDict]];
     
-    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.stepsBar attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBaseline multiplier:1 constant:44];
-    [self.view addConstraint:constraint];
+    if (@available(iOS 11.0, *)) {
+        [self.stepsBar.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:0].active = YES;
+    } else {
+        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(0)-[stepsBar]" options:0 metrics:nil views:bindingsDict]];
+        NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.stepsBar attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.topLayoutGuide attribute:NSLayoutAttributeBaseline multiplier:1 constant:44];
+        [self.view addConstraint:constraint];
+    }
     
     [self loadStepViewControllers];
     [self showStepViewController:[self.childViewControllers objectAtIndex:0] animated:NO];
